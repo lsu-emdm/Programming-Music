d14fd92cc704ea3be0c4fbbc21cb6102287ce194
Zak Berkowitz <zberko1@tigers.lsu.edu>
2015-07-28 22:25:10 -0500

    Add FM and Filtering

--- SoundBasics/SoundBasics.js
+++ SoundBasics/SoundBasics.js
@@ -2,11 +2,15 @@ var scope1 = new WavyJones(Tone.context, 'oscilloscope1');
 var scope2 = new WavyJones(Tone.context, 'oscilloscope2');
 var scope3 = new WavyJones(Tone.context, 'oscilloscope3');
 var scope4 = new WavyJones(Tone.context, 'oscilloscope4');
+var scope5 = new WavyJones(Tone.context, 'oscilloscope5');
+var scope6 = new WavyJones(Tone.context, 'oscilloscope6');
 
 scope1.connect(Tone.Master);
 scope2.connect(Tone.Master);
 scope3.connect(Tone.Master);
 scope4.connect(Tone.Master);
+scope5.connect(Tone.Master);
+scope6.connect(Tone.Master);
 
 var sineOsc = new Tone.Oscillator({
 	frequency: 440,
@@ -18,6 +22,18 @@ var sawOsc = new Tone.Oscillator({
 	frequency: 440,
 	volume: -12
 }).connect(scope2);
+
+var lowPassFilter = new Tone.Filter({
+	type: "lowpass",
+	frequency: 5000,
+	Q: 10
+}).connect(scope5);
+
+var filterOsc = new Tone.Oscillator({
+	type: "sawtooth",
+	frequency: 440,
+	volume: -12
+}).connect(lowPassFilter);
 
 var phaseOsc1 = new Tone.Oscillator({
 	frequency: 440,
@@ -40,6 +56,17 @@ var beatOsc2 = new Tone.Oscillator({
 	frequency: 441,
 	volume: -12
 }).connect(scope4);
+
+var fmSynth = new Tone.FMSynth({
+	harmonicity: 0,
+	modulationIndex: 0,
+	modulator: {
+		oscillator: {
+			type: "sine"
+		}
+	}
+}).connect(scope6);
+
 
 nx.onload = function(){
 	nx.sendsTo("js");
@@ -69,12 +96,23 @@ nx.onload = function(){
 		value: 1
 	}, true);
 
+
+	cutoffFrequencyDial.set({
+		value: 1
+	}, true);
+
+
 	phaseSlider.set({
 		value: 0
 	}, true);
 
 	beatFreqNumber.set({
 		value: 1
+	}, true);
+
+	fmPosition.set({
+		x: 0,
+		y: 0
 	}, true);
 
 	sineToggle.on('*', function(data){
@@ -109,6 +147,20 @@ nx.onload = function(){
 		if (data.value >= 1){
 			sawOsc.type = "sawtooth" + (data.value + 1);
 		}
+	});
+
+	filterToggle.on('*', function(data){
+		if (data.value == 1){
+			filterOsc.start();
+		}else if (data.value == 0){
+			filterOsc.stop();
+		}
+	});
+
+	cutoffFrequencyDial.on('*', function(data){
+		var cutoff = Math.floor(data.value * 5000);
+		lowPassFilter.frequency.value = cutoff;
+		document.getElementById("cutoffLabel").innerHTML = "Cutoff Frequency: " + cutoff + " Hz";
 	});
 
 	phaseSineToggle.on('*', function(data){
@@ -159,5 +211,21 @@ nx.onload = function(){
 
 	beatFreqNumber.on('*', function(data){
 		beatOsc2.frequency.value = beatOsc1.frequency.value + data.value;
+	});
+
+	fmToggle.on('*', function(data){
+		if (data.value == 1){
+			fmSynth.triggerAttack("A4");
+		}else if (data.value == 0){
+			fmSynth.triggerRelease();
+		}
+	});
+
+	fmPosition.on('*', function(data){
+		var index = data.y * 10;
+		var harmonicity = data.x * 3;
+
+		fmSynth.modulationIndex.value = index;
+		fmSynth.harmonicity.value = harmonicity;
 	});
 }
